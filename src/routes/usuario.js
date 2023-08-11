c76670d2a7b1d9600ca8ef332280eea361ec0f70
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-08-10 22:02:52 -0300

    nova rota login do usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,7 @@ const express = require('express');
 const conectarBancoDados = require('../midlewares/conectarBD');
 const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 const EsquemaUsuario = require('../models/usuario');
 const router = express.Router();
 
@@ -24,6 +25,36 @@ router.post('/criar', conectarBancoDados, async function(req, res) {
       return tratarErrosEsperados(res, "Error: Já existe uma conta com esse e-mail!")
     }
     return tratarErrosEsperados(res, error)
+  }
+});
+
+router.post('/logar', conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { email, senha } = req.body;
+
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select('+senha');
+    if (respostaBD) {
+
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        
+        let token = jwt.sign({ id: respostaBD._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+
+        res.header('x-auth-token', token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso.",
+          resposta: { "x-auth-token": token }
+        });
+      } else {
+        throw new Error("Email ou senha incorreta");
+      }
+    } else {
+      throw new Error("Email ou senha incorreta");
+    }
+  } catch (err) {
+    return tratarErrosEsperados(res, err);
   }
 });
 
